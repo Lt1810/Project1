79aaa7c7a6eb7ec554a25d0e7f5f5cee0a03c63a
Luis T <luisftrejo1@gmail.com>
2024-06-03 10:14:03 -0700

    #6/2

--- src/demo1/practice1.java
+++ src/demo1/practice1.java
@@ -5,5 +5,7 @@ public static void main(String[] args) {
 	System.out.println("test");
 	System.out.println("GetHub Lecture...");
 	System.out.println("GetHub Lecture 2...");
+	System.out.println("GetHub Lecture 3...");
+	System.out.println("GetHub Lecture 4...");
 }
 }
