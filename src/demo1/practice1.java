e40ac1e3cc582f0e52297d6c258d6d0112e0b4d6
Luis <Luisftrejo1@gmail.com>
2024-06-07 19:27:15 -0700

    Finally this time

--- src/demo1/practice1.java
+++ src/demo1/practice1.java
@@ -7,5 +7,7 @@ public static void main(String[] args) {
 	System.out.println("GetHub Lecture 2...");
 	System.out.println("GetHub Lecture 3...");
 	System.out.println("GetHub Lecture 4...");
+	System.out.println("Finaly...");
+
 }
 }
